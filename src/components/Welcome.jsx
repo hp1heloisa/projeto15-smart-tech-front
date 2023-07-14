0467c12b1723327473ad52a42125acc80d06324b
hp1heloisa <heloisapimentel2202@gmail.com>
2023-07-13 22:08:32 -0300

    navbar alterada

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,51 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+export default function Welcome() {
+
+    const navigate = useNavigate(true);
+
+    let data = JSON.parse(localStorage.getItem("dataSmartTech"));
+    if (!data) {
+        return(
+            <div className="signIn-Out" onClick={() => navigate('/cadastro')}>
+                <Profile>
+                    <ion-icon name="person-sharp"></ion-icon>
+                </Profile>
+                <p>
+                    Faça <span>LOGIN</span> ou <br />
+                    crie seu <span>CADASTRO</span>
+                </p>
+            </div>
+        )
+    } else {
+        return(
+            <div className="signIn-Out" onClick={() => navigate('/cadastro')}>
+                <Profile>
+                    <ion-icon name="person-sharp"></ion-icon>
+                </Profile>
+                <p>
+                    Bem-vindo(a), <span>{data.name.toUpperCase()}</span>
+                </p>
+            </div>
+        )
+    }
+}
+
+const Profile = styled.div`
+width: 30px;
+height: 30px;
+border: 2px solid #f3f3f3;
+border-radius: 100%;
+overflow: hidden;
+
+display: flex;
+align-items: center;
+justify-content: center;
+
+ion-icon {
+  width: 15px;
+  height: 15px;
+  color: #f3f3f3;
+}
+`;
