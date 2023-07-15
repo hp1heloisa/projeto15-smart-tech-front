694347785fcb77fd9630886166babfab2405a664
Brendo <brendow_prime@hotmail.com>
2023-07-14 22:05:34 -0400

    Banner inserido na HomePage

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -20,22 +20,23 @@ export default function ProductCard(props) {
 
 const ProductItem = styled.div`
     background-color: #ffffff;
-    height: 480px;
-    width: 250px;
+    height: 500px;
+    width: 300px;
     padding: 20px;
+    padding-left: 30px;
     border-radius: 15px;
     box-shadow: 3px 5px 5px #a7a7a7;
     display: flex;
     flex-direction: column;position: relative;
     img{
-        width: 210px;
-        height: 210px;
+        width: 200px;
+        height: 250px;
         margin-bottom: 20px;
         border-radius: 15px;
         align-items: center;
     }h1{
         font-family: "Poppins";
-        font-size: 20px;
+        font-size: 18px;
         font-weight: 500;
         font-weight: bold;
         letter-spacing: 2px;
@@ -48,12 +49,12 @@ const ProductItem = styled.div`
         font-weight: bold;
         margin-bottom: 24px;
         position: absolute;
-        bottom:73px;
+        bottom:80px;
     }
 `
 const ButtonCart = styled.button`
     background-color: #fda42f;
-    width: 210px;
+    width: 238px;
     height: 55px;
     display: flex;
     justify-content: center;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import ProductCard from "../components/ProductCard.jsx"
 import { useState } from "react"
 import { useEffect } from "react"
 import api from "../services/api.js"
+import Banner from "../assets/banner.png"
 
 
 export default function HomePage() {
@@ -48,7 +49,9 @@ export default function HomePage() {
     return (
         <>
             <NavBar />
+            
             <HomePageComponent>
+                <BannerImage src={Banner} alt="banner-img"/>
                 <ProductsArea>
                     {products.map((prod) => <ProductCard key={prod._id} product={prod} />)}
                 </ProductsArea>
@@ -67,15 +70,20 @@ const HomePageComponent = styled.div`
     align-items: center;
     box-shadow: 3px 5px 5px #a7a7a7;
     `
+
+    const BannerImage = styled.img`
+        width: auto;
+        height: auto;
+    `
+
 const ProductsArea = styled.div`
     background-color: #e4e4e4;
     width: 100%;
     height: 100%;
-    margin-top: 120px;
     margin-bottom: 120px;
     padding-top: 70px;  
     padding-bottom: 70px;
-    gap: 70px;
+    gap: 50px;
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
